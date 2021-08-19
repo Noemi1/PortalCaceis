12b64cc742ab712e36b5063680a1041e15f7dd95
Noemi <Noemi@DESKTOP-HVURGOU>
2021-08-19 17:29:29 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './parts/footer/footer.component';
+import { HeaderComponent } from './parts/header/header.component';
+import { HomeComponent } from './parts/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
