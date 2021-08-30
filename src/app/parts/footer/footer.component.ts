f16063edbbdfcd0dceb1d85e1990895605f76b2f
Noemi <Noemi@DESKTOP-HVURGOU>
2021-08-30 10:33:39 -0300

    header/footer

--- src/app/parts/footer/footer.component.ts
+++ src/app/parts/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Role } from 'src/app/models/role';
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  year = new Date().getFullYear();
+  Role = Role;
+  isLoggedIn = false;
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
